76aa592d933728815db18f5badc17ba94781852f
oribelloryan <oribelloryan@gmail.com>
2016-12-28 03:12:55 +0800

    New addition

--- plotting.php
+++ plotting.php
@@ -72,13 +72,18 @@
      directionsDisplay = new google.maps.DirectionsRenderer;
     map = new google.maps.Map(document.getElementById('map'), {
     zoom: minZoomLevel,
+    scrollwheel: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    draggable: false,
     center: centeroftheearth ,
     mapTypeId: 'roadmap'
 
     
     });
     
-    var flightPlanCoordinates = [
+    var boundaries = [
           {lat: 14.601600, lng: 121.059246 },
           {lat: 14.600801, lng: 121.056767 },
           {lat: 14.600775, lng: 121.055544 },
@@ -89,17 +94,102 @@
           {lat: 14.592989, lng: 121.041307 },
           {lat: 14.592164, lng: 121.041601 },
           {lat: 14.592166, lng: 121.038099 },
-          {lat: 14.590712, lng: 121.034731 }
+          {lat: 14.590712, lng: 121.034731 },
+          {lat: 14.590977, lng: 121.034548 }, 
+          {lat: 14.591346, lng: 121.034167 },
+          {lat: 14.591645, lng: 121.033971 },
+          {lat: 14.591704, lng: 121.033672 },
+          {lat: 14.591675, lng: 121.033312 },
+          {lat: 14.591854, lng: 121.033215 },
+          {lat: 14.592585, lng: 121.032479 },
+          {lat: 14.592321, lng: 121.031697 },
+          {lat: 14.592820, lng: 121.031841 },
+          {lat: 14.593284, lng: 121.032115 },
+          {lat: 14.593433, lng: 121.031894 },
+          {lat: 14.593903, lng: 121.031376 },
+          {lat: 14.593935, lng: 121.030824 },
+          {lat: 14.594168, lng: 121.030589 },
+          {lat: 14.594322, lng: 121.030133 },
+          {lat: 14.594619, lng: 121.029838 },
+          {lat: 14.594839, lng: 121.029451 },
+          {lat: 14.595203, lng: 121.029121 },
+          {lat: 14.595016, lng: 121.028620 },
+          {lat: 14.595908, lng: 121.027038 },
+          {lat: 14.595159, lng: 121.026355 },
+          {lat: 14.597218, lng: 121.025411 },
+          {lat: 14.597361, lng: 121.025240 },
+          {lat: 14.597945, lng: 121.024796 },
+          {lat: 14.599388, lng: 121.024057 },
+          {lat: 14.601612, lng: 121.020836 },
+          {lat: 14.602053, lng: 121.020017 },
+          {lat: 14.602262, lng: 121.019835 },
+          {lat: 14.602647, lng: 121.019744 },
+          {lat: 14.603716, lng: 121.020062 },
+          {lat: 14.604409, lng: 121.020301 },
+          {lat: 14.604974, lng: 121.020725 },
+          {lat: 14.607852, lng: 121.022667 },
+          {lat: 14.608851, lng: 121.022263 },
+          {lat: 14.609448, lng: 121.021589 },
+          {lat: 14.609535, lng: 121.021455 },
+          {lat: 14.612066, lng: 121.023138 },
+          {lat: 14.613488, lng: 121.023318 },
+          {lat: 14.613727, lng: 121.023475 },
+          {lat: 14.613825, lng: 121.023901 },
+          {lat: 14.613325, lng: 121.025024 },
+          {lat: 14.612424, lng: 121.025327 },
+          {lat: 14.612229, lng: 121.026325 },
+          {lat: 14.612272, lng: 121.026875 },
+          {lat: 14.612250, lng: 121.027852 },
+          {lat: 14.611990, lng: 121.028738 },
+          {lat: 14.612201, lng: 121.029152 },
+          {lat: 14.612729, lng: 121.029234 },
+          {lat: 14.612977, lng: 121.029338 },
+          {lat: 14.613134, lng: 121.029710 },
+          {lat: 14.613100, lng: 121.030105 },
+          {lat: 14.612943, lng: 121.030454 },
+          {lat: 14.612898, lng: 121.030884 },
+          {lat: 14.611931, lng: 121.031372 },
+          {lat: 14.611672, lng: 121.031674 },
+          {lat: 14.611290, lng: 121.032232 },
+          {lat: 14.610865, lng: 121.032662 },
+          {lat: 14.610588, lng: 121.032949 },
+          {lat: 14.609675, lng: 121.033618 },
+          {lat: 14.609397, lng: 121.034167 },
+          {lat: 14.609397, lng: 121.034777 },
+          {lat: 14.608796, lng: 121.035254 },
+          {lat: 14.608553, lng: 121.035756 },    
+          {lat: 14.608646, lng: 121.036210 },
+          {lat: 14.608634, lng: 121.036318 },
+          {lat: 14.608484, lng: 121.036592 },
+          {lat: 14.608403, lng: 121.036843 },
+          {lat: 14.607871, lng: 121.036891 },
+          {lat: 14.607513, lng: 121.037154 },
+          {lat: 14.606634, lng: 121.037285 },
+          {lat: 14.606249, lng: 121.037752 },
+          {lat: 14.608734, lng: 121.040903 },
+          {lat: 14.609585, lng: 121.043059 },
+          {lat: 14.608918, lng: 121.044077 },
+          {lat: 14.608617, lng: 121.045784 },
+          {lat: 14.609635, lng: 121.049993 },
+          {lat: 14.604469, lng: 121.052115 },
+          {lat: 14.606622, lng: 121.056957 },
+          {lat: 14.606622, lng: 121.056984 },
+          {lat: 14.601611, lng: 121.059250 },
         ];
-    var flightPath = new google.maps.Polyline({
-          path: flightPlanCoordinates,
-          geodesic: true,
-          strokeColor: '#FF0000',
-          strokeOpacity: 1.0,
-          strokeWeight: 2
+
+
+    var sanJuanBoundaries = new google.maps.Polygon({
+          paths: [boundaries],
+          strokeColor: '#FFC107',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FFC107',
+          fillOpacity: 0.35
         });
 
-    flightPath.setMap(map);
+    sanJuanBoundaries.setMap(map);
+
+
     directionsDisplay.setMap(map);
     var strictBounds = new google.maps.LatLngBounds(
     new google.maps.LatLng(14.600561, 121.036487),
@@ -230,7 +320,7 @@
    }
     </script>
     <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD1SFa75QzMfOtf7rudCh6RFgaNk6ptbzo&libraries=drawing&libraries=geometry&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD1SFa75QzMfOtf7rudCh6RFgaNk6ptbzo&libraries=geometry&callback=initMap">
     </script>
     <!-- Bootstrap core JavaScript
     ================================================== -->
