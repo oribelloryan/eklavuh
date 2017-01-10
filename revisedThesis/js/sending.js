bdeda123d8b58c450adca5122f6fb974ce2e59b2
oribelloryan <oribelloryan@gmail.com>
2017-01-10 18:09:23 +0800

    ajax function

--- revisedThesis/js/sending.js
+++ revisedThesis/js/sending.js
@@ -2,7 +2,7 @@ function sendingData(location, checkpoints){
     var checkpoint = JSON.stringify(checkpoints);
     var target = JSON.stringify(location);
     $.ajax({
-        url: 'storing.php?checkpoints='+checkpoint+'&target='+target,
+        url: 'server_storing.php?checkpoints='+checkpoint+'&target='+target,
         type: 'GET',
         success: function(results) {
         }
